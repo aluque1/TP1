df56e913aff35fbd00438bfd1d6625a21f4d4633
Aluque <32274359+aluque1@users.noreply.github.com>
2021-09-30 11:20:46 +0200

    Cambios al Controller
    Lo que hemos estado hablando en la practica

--- practica1/src/es/ucm/tp1/control/Controller.java
+++ practica1/src/es/ucm/tp1/control/Controller.java
@@ -1,11 +1,14 @@
 package es.ucm.tp1.control;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 import es.ucm.tp1.logic.Game;
 
 public class Controller {
+	
+	// Attributes ---------------------------------------------------
+	private boolean exit;
+	private boolean passTurn;
 
 	private static final String PROMPT = "Command > ";
 
@@ -41,16 +44,76 @@ public class Controller {
 		String line;
 		String[] words;
 		
-		line = scanner.nextLine();
-		words = line.toLowerCase().trim().split("\\s+");
-		
-		switch(words[0].charAt(0)) {
-		case 'h':
+		while (!exit) {
+			// We use this to parse the command so it's all in the same format and then we divide the line entered in the console to and array of words
+			System.out.print(PROMPT);
+			line = scanner.nextLine();
+			words = line.toLowerCase().trim().split("\\st");
 			
+			// We add a switch statement to create the command options selection
+			switch (words[0]) {
+			
+			// Prints the available commands
+			case "help" : case "h":
+				for(int i = 0; i < HELP.length; i++)
+					System.out.println(HELP[i]);
+				passTurn = false;
+				break;
+				
+			// Prints the gameobject info
+			case "info" : case "i" :
+				
+				// TODO Print gameobject info
+				passTurn = false;
+				break;
+				
+			// Passes one game cycle without moving UP or DOWN, just FORWARD 1 cell
+			case "none" : case "n" : case "":
+				// TODO Passes one game cycle without moving up or down
+				passTurn = true;
+				break;
+			
+			// Passes one game cycle and moves the car UP 1 cell
+			case "q" :
+				// TODO Passes one game cycle and moves the car UP 1 cell
+				passTurn = true;
+				break;
+			
+			//Passes one game cycle and moves the car DOWN 1 cell
+			case "a" :
+				// TODO Passes one game cycle and moves the car DOWN 1 cell.
+				passTurn = true;
+				break;
+			
+			// Exits the game
+			case "exit" : case "e" :
+				exit = true;
+				passTurn = false; // Don't know if this is needed or not 
+				break;
+			
+			// Resets the game with the same LEVEL and SEED parameters as before
+			case "reset": case "r" :
+				// TODO Reset the Game and start a new one with the same LEVEL and SEED 
+				passTurn = false; // Don't know if this is needed or not 
+				break;
+			
+			// Enables test mode
+			case "test" : case "t" :
+				// TODO Enable test mode (whatever that is.)
+				passTurn = false; // Don't know if this is needed or not 
+				break;
+			
+			// In the default case we let the Player know that there has been an error trying to parse the command and doesn't pass a turn.
+			default:
+				System.out.println("[ERROR]" + UNKNOWN_COMMAND_MSG);
+				passTurn = false;
+				break;
+			}
+			
+			//This if is to check if we have to update the game board as there are some command that don't advance one step in the game
+			//if(passTurn)
+				//this.game.update();
 		}
-		
-		
-		
 	}
 
 }
