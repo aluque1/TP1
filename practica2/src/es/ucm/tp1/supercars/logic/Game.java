7c300f08600552a49f59422d594e1ba9deac3310
Aluque <32274359+aluque1@users.noreply.github.com>
2021-12-16 12:07:23 +0100

    Cosas que tenemos que cambiar para la version final del examen

--- practica2/src/es/ucm/tp1/supercars/logic/Game.java
+++ practica2/src/es/ucm/tp1/supercars/logic/Game.java
@@ -12,6 +12,10 @@ import es.ucm.tp1.supercars.control.exceptions.InputOutputRecordException;
 import es.ucm.tp1.supercars.logic.gameobjects.GameObject;
 
 /** TODO List
+ * 
+ * En todos los comandos lanzamos un try catch(); 
+ * Hacer todos los throws desde el game. (Como is fuera un execute)
+ * throw en los try catch salvo el def controller
  */
 public class Game{
 
